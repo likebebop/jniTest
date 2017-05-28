26c79f57f88eca0f31f0f78b1da5251e00b45173
Byung Woong Kwon <likebebop0@gmail.com>
2017-05-28 10:48:35 +0900

    test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,13 +1,31 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
+#include "json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_example_likebebop_jnitest_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+    json test;
+
+    json j = "{ \"happy\": true, \"pi\": 3.141 }"_json;
+
+    //__android_log_print(ANDROID_LOG_ERROR, "TRACKERS", "happy = %d", j["happy"]);
+
+    __android_log_print(ANDROID_LOG_ERROR, "TRACKERS", "happy = %d", false);
+    // j["happy"] = true;
+
+    __android_log_print(ANDROID_LOG_ERROR, "TRACKERS", "happy = %d, %.3f", (bool)j["happy"], (float)j["pi"]);
+
+    __android_log_print(ANDROID_LOG_ERROR, "TRACKERS", "happy =  %.3f", j.at("pi").get<float>());
+
+    return env->NewStringUTF(j.dump().c_str());
 }
